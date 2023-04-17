37d34825f66ba241e2f7ee529a0f6ffa86607a45
Hd Lg <59037834+Hd-Lg@users.noreply.github.com>
2023-04-17 02:36:45 +0200

    Done Camera, Mesh

---
+++ client/src/canvas/Backdrop.jsx
@@ -0,0 +1,10 @@
+import { easing } from 'maath';
+import { useFrame } from '@react-three/fiber';
+import { useRef } from 'react';
+import { AccumulativeShadows, RandomizedLight } from '@react-three/drei';
+
+const Backdrop = () => {
+  return <div>Backdrop</div>;
+};
+
+export default Backdrop;
